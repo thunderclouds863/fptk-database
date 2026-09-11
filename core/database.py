065a9a4f13128dbe26e8884951746e365b30182b
Desi Armanda Sari <desi.arr863@gmail.com>
2026-09-11 07:27:09 +0700

    Update database.py

--- core/database.py
+++ core/database.py
@@ -17,12 +17,14 @@ engine = create_engine(DATABASE_URL, pool_pre_ping=True)
 SessionLocal = sessionmaker(bind=engine, autocommit=False, autoflush=False)
 Base = declarative_base()
 
+
 def get_db():
     db = SessionLocal()
     try:
         yield db
     finally:
         db.close()
+
 
 def init_db():
     """Create all tables if they don't exist"""
